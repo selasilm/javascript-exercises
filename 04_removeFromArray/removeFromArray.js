4667ac8640a203da8065dc9034e6945e61f6e887
Selasi Mensah <selasilm@gmail.com>
2025-01-21 13:03:59 +0000

    tests passed by deleting items and filtering

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(array,...args) {
+    let len = array.length;
+    let argLen = args.length;
+    let newArray=[];
+    for(let i=0; i<len;i++){
+        for (let j=0;j<argLen;j++){
+            if (array[i]===args[j]){
+                delete array[i];
+            }
+        }
+    }
+    array = array.filter(item=>item !==undefined);
+    return array;
 };
 
 // Do not edit below this line
