e85df9ce530f13e4657ce81c9e92a075b2ecc7dd
Selasi Mensah <selasilm@gmail.com>
2025-01-21 13:25:00 +0000

    all tests passed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(start,end) {
+    let sum = 0;
+    if (start <0||end<0){
+        return 'ERROR';
+    }
+    if (!Number.isInteger(start)||!Number.isInteger(end)){
+        return 'ERROR';
+    }
+    if(Number.isInteger(start) && Number.isInteger(end)){
+        for (let i=Math.min(start,end);i<=Math.max(start,end);i++){
+            sum+=i;
+        }
+        return sum;
+    }
 };
 
 // Do not edit below this line
