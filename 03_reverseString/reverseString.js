3b039d015c080c7d283e963e1db69131b369940d
Selasi Mensah <selasilm@gmail.com>
2025-01-21 12:12:27 +0000

    all tests passed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    const array = word.split("");
+    let rev = "";
+    let len = array.length-1
+    for(let i=0; i<=len;i++){
+        rev += array[len-i];
+    }
+    return rev;
 };
 
 // Do not edit below this line
