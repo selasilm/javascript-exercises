85d795d0124881fbcce3c9c921209d6f16a9fcd0
Selasi Mensah <selasilm@gmail.com>
2025-01-21 12:00:46 +0000

    tests passed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(add, num) {
+    if(num<0) return "ERROR";
+    let str="";
+    for(let i =0;i<num;i++){
+        str += add;
+    }
+    return str;
 };
 
 // Do not edit below this line
