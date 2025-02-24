d9472dad71f011ba951644ad5154c34f007cbe7b
Selasi Mensah <selasilm@gmail.com>
2025-02-24 17:06:11 +0000

    all tests passed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let lower = str.toLowerCase();
+    let raw = lower.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+    let ns = raw.replace(/\s/g,'');
+    let rev = ""
+    let arr = ns.split('');
+    let len = arr.length-1;
+    for(let i=0;i<=len;i++){
+        rev += arr[len-i];
+    }
+    if(rev===ns){
+        return true;
+    }else{
+        return false;
+    }
 };
 
 // Do not edit below this line
