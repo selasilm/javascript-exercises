c620d17b614943b7c24e129caea1a4eca4638f83
Selasi Mensah <selasilm@gmail.com>
2025-02-24 17:48:55 +0000

    all tests passed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    num = parseInt(num);
+    if (num >= 2){
+        return fibonacci(num-1)+fibonacci(num-2);
+    }else if (num < 0){
+        return "OOPS";
+    }else{
+        return num;
+    }
 };
 
 // Do not edit below this line
