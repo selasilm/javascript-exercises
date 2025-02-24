a5c013f5b03235d94f5c92d9d5ea7f3ce5983070
Selasi Mensah <selasilm@gmail.com>
2025-02-24 16:45:29 +0000

    exercise 8 completed

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
-	
+const add = function(num1,num2) {
+	return num1+num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1,num2) {
+	return num1-num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let total=0;
+	for(let i=0;i<=arr.length-1;i++){
+    total += arr[i];
+  }
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let prod = 1;
+  for(let i=0;i<=arr.length-1;i++){
+    prod *= arr[i];
+  }
+  return prod;
 };
 
-const power = function() {
-	
+const power = function(num1,num2) {
+  let result=1;
+	for (let i=0;i<=num2-1;i++){
+    result*=num1;
+  }
+  return result
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num >1){
+    return num * factorial(num-1)
+  }
+  else{
+    return 1;
+  }
 };
 
 // Do not edit below this line
